9476e6106d2077eb6fb3040a5d7910c3825d1a5d
Mario Bielert <mario.bielert@tu-dresden.de>
2018-06-26 17:37:17 +0200

    Adds missing CMake boilerplate magic file

---
+++ x86_energyConfig.cmake
@@ -0,0 +1,10 @@
+include("${CMAKE_CURRENT_LIST_DIR}/ix86_energyTargets.cmake")
+
+get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
+get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
+get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
+get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
+
+message(STATUS "Using x86_energy: ${_IMPORT_PREFIX}")
+
+set(_IMPORT_PREFIX)
