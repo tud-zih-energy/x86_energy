48b0a420e7c8743f2ea235979daded52448ced32
Mario Bielert <mario.bielert@tu-dresden.de>
2018-07-16 09:03:15 +0200

    This is actually not required, if you're holding CMake the right way.

--- x86_energyConfig.cmake
+++ x86_energyConfig.cmake
@@ -1,7 +1,5 @@
 include("${CMAKE_CURRENT_LIST_DIR}/x86_energyTargets.cmake")
 
-set(x86_energy_FOUND TRUE)
-
 get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
 get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
 get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
